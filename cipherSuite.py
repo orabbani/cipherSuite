a592346cb7cce5c5a6907711c121713015aeeb11
KM <slinkyfromhell@gmail.com>
2019-12-18 21:37:11 -0800

    -Deleted all files and started cipherSuite.py from scratch. -Ported caesarCipher.py and transpositionCipher.py as classes Caesar and Transposition respectively to cipherSuite.py Usage: from cipherSuite import Caesar ''' key is optional, when omitted a random key is generated modes:  'e':'encrypt', 'd':'decrypt' ''' c = Caesar(message='<message>', key=42, mode='e') t = c.translation k = c.getKey()

--- cipherSuite.py
+++ cipherSuite.py
@@ -1,34 +1,175 @@
-import sys, os
-import paths # path[folder], writePathsToPy()
-paths.writePathsToPy()
+# cipherSuite.py
+# Author: slinkyfromhell
+# Description:
+#   Classes and methods pertaining to simple cipher techniques.
+#   Only provides minimal input validation. A bad key will result
+#       in an object not instantiating correctly. It is recommended
+#       to allow the library to generate a key upon encryption, and to
+#       use <Cipher>.getKey() to retrieve it for decryption.
 
-# Third party modules
-import pyperclip
+'''
+# CLASS CAESAR
+# One liner print:
+    print(Caesar(message='test message', key=1, mode='e').translation)
+# Intended usage:
+    c = Caesar(message, mode='e')
+    k = c.getKey()
+    t = c.translation
+# Additional parameters:
+    Caesar(message, key, mode)
+    modes = {
+        'e':'encrypt',
+        'd':'decrypt'
+    }
+'''
 
-# symbols is a base dependency and should go early in the load order
-import symbols # get()
-SYMBOLS = symbols.get()
+import sys, os, secrets, string, math
 
-# message is necessary for ciphers
-import message as me # getInput(), getFile(), getFile_(path), isValid(message)
+CHARSET = string.ascii_letters + ' ' + string.digits
+class Caesar:
+    class Key:
+        # Random key generator
+        def generate(self):
+            self.set(secrets.randbelow(self.limit-1) + 1)
+        # Key variable I/O
+        def print(self): print('KEY_CAE:', self.key)
+        def get(self): return self.key
+        # If keyVal is valid returns 1, else returns 0
+        def set(self, keyVal):
+            if keyVal <= self.limit and keyVal > 0:
+                self.key = keyVal
+                return 1
+            else: return 0
 
-# ciphers here. doesn't matter for now what order they're in
-import caesarCipher as cc # getKey(), encrypt(message, key), decrypt(message, key)
-import transpositionCipher as tc # getKey(message), encrypt(message, key), decrypt(message, key)
+        def __init__(self, key=None, v=True):
+            # Set the upper value limit for the key
+            self.limit = len(CHARSET)
+            # Check if key var was passed
+            if key:
+                # Passes to set. Does nothing if accepted
+                if self.set(key): None
+                # If key was out of bounds, generate a new key and print
+                else:
+                    if v: print('ERR_CAE: Bad key given. Generating a new one')
+                    self.generate()
+                    if v: self.print()
+            # If var was not passed, generate a key
+            else: self.generate()
 
-# this is a utility and hasn't been implemented yet
-import detectWords as dw # isValid(message, wordPercentage, lettersPercentage)
+    # Access Key variables
+    def getKey(self): return self.key.get()
+    def setKey(self, key): self.key.set(key)
+    def getKeyLimit(self): return self.key.limit
+
+    # Encrypts the message and sets Caesar.translation
+    def encrypt(self):
+        translation = ''
+        for c in self.message:
+            charIndex = CHARSET.find(c)
+            translatedIndex = charIndex + self.getKey()
+            if translatedIndex >= self.getKeyLimit():
+                translatedIndex -= self.getKeyLimit()
+            translation += CHARSET[translatedIndex]
+        self.translation = translation
+
+    # Decrypts the message and sets Caesar.translation
+    def decrypt(self):
+        translation = ''
+        for c in self.message:
+            charIndex = CHARSET.find(c)
+            translatedIndex = charIndex - self.getKey()
+            if translatedIndex < 0:
+                translatedIndex += self.getKeyLimit()
+            translation += CHARSET[translatedIndex]
+        self.translation = translation
+
+    def __init__(self, message=None, key=None, mode=None):
+        if message: self.message = message
+        if key: self.key = self.Key(key)
+        else: self.key = self.Key()
+        
+        if mode:
+            if mode == 'e':
+                if not message: print('ERR_CAE_INIT: No message given.')
+                else: self.encrypt()
+            elif mode == 'd':
+                if not message: print('ERR_CAE_INIT: No message given.')
+                elif not key: print('ERR_CAE_INTI: No key given.')
+                else: self.decrypt()
+            else: print('ERR_CAE_INIT: Invalid mode given.')
+            
 
 
+class Transposition:
+    class Key:
+        def generate(self, message):
+            self.key = secrets.randbelow(self.limit - 2) + 2
+
+        def print(self): print('KEY_TRA:', self.key)
+
+        def get(self): return self.key
+        def set(self, key):
+            if key > 2 and key < self.limit:
+                self.key = key
+                return 1
+            else: return 0
+ 
+        def __init__(self, message, key=None, v=True):
+            self.limit = math.floor(len(message) / 2)
+            if key:
+                # Passes to set. Does nothing if accepted
+                if self.set(key): None
+                # If key was out of bounds, generate a new key and print
+                else:
+                    if v: print('ERR_TRA: Bad key given. Generating a new one')
+                    self.generate(message)
+                    if v: self.print()
+            # If var was not passed, generate a key
+            else: self.generate(message)
+
+    def getKey(self): return self.key.get()
+    def setKey(self, key): self.key.set(key)
+    def getKeyLimit(self): return self.key.limit
+
+    def encrypt(self):
+        translation = [''] * self.getKey()
+        for column in range(self.getKey()):
+            index = column
+            while index < len(self.message):
+                translation[column] += self.message[index]
+                index += self.getKey()
+        self.translation = ''.join(translation)
+
+    def decrypt(self):
+        columns = int(math.ceil(len(self.message) / float(self.getKey())))
+        rows = self.getKey
+        shadows = (columns * rows) - len(self.message)
+        translation = [''] * columns
+        column = 0
+        row = 0
+        for c in self.message:
+            translation[column] += c
+            column += 1
+            if (column == columns) or \
+                (column == columns - 1 and row >= rows - shadows):
+                column = 0
+                row += 1
+        self.translation = ''.join(translation)
+
+    def __init__(self, message, key=None, mode=None):
+        self.message = message
+        if key: self.key = self.Key(message=message, key=key)
+        else: self.key = self.Key(message=message)
+        if mode and message and key:
+                if mode == 'e': self.encrypt()
+                elif mode == 'd': self.decrypt()
+                else: print('ERR_TRA_INIT: Invalid mode given')
+        if mode and not message:
+            print('ERR_TRA_INIT: Missing argument: message')
+        if mode and not key:
+            print('ERR_TRA_INIT: Missing argument: key')
 
 
-
-
-
-
-
-def test():
-    print('test')
-
-if __name__ == '__main__':
-    print('Cipher Suite') 
+if __name__=='__main__':
+    c = Caesar('this is a test string', key=1, mode='e')
+    print(c.translation)
